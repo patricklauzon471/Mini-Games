3779a40690a08ab6cc9c65188383c6feac0344ae
patricklauzon471 <83890368+patricklauzon471@users.noreply.github.com>
2022-11-09 22:10:21 -0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,47 @@
+# Calculator
+
+# Add
+def add(n1, n2):
+    return n1 + n2
+# Subtract
+
+
+def subtract(n1, n2):
+    return n1 - n2
+# Multiply
+
+
+def multiply(n1, n2):
+    return n1 * n2
+# Divide
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {"+": add, "-": subtract, "*": divide, "/": divide}
+
+
+def calculator():
+    num1 = int(input("What is the first number?: "))
+    for key in operations:
+        print(key)
+
+    should_continue = True
+    while should_continue == True:
+        operation_symbol = input(
+            "Pick an operation symbol from the line above: ")
+        num2 = int(input("What is the second number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue alculating with {answer} or type 'n' to exit: ") == 'y':
+            num1 = answer
+        else:
+            should_continue == False
+            calculator()
+
+
+calculator()
